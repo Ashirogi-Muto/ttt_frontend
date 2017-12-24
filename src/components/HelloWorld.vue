21690c74dcdbadd4387b7853aed553663bf9e9e1
Kshitij Pandey <ashirogi@Kshitijs-MacBook-Air.local>
2017-12-25 00:45:51 +0530

    initial setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="hello">
+	
+</template>
+
+<script>
+export default {
+	name: 'HelloWorld',
+	data () {
+		return {
+			msg: 'Welcome to Your Vue.js App'
+		}
+	}
+}
+</script>
